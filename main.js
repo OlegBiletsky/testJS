b4fe0f12072dbd98792700863fd7e32b212a5129
OlegBiletsky <ol.biletsky@gmail.com>
2020-10-12 21:25:24 +0300

    refac

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let maxEl = arr1.reduce(function(a, b) {
     return Math.max(a, b);
 });
 
-console.log(maxEl);
+console.log(maxEl, `maxEl in [${arr1}]`);
 
 
 
@@ -23,7 +23,7 @@ console.log(arrNew);
 
 
 // 3.	Дано колекцію - [{name: "Yura", age: 55, hobby: ["football", "ski", "hiking"], type: "Admin"}, {name: "Yulian", age: 28, hobby: ["films", "games", "hiking"], type: "user"}, {name: "Taras", age: 38, hobby: ["hunting", "TV", "javascript"], type: "user"}] – Вивести всіх юзерів з типом user. Вивести юзерів які мають хоббі hiking. Додати можливість додати нове поле для всіх юзерів – job (true/false);
-/*
+
 let Peoples = [
     {name: "Yura", age: 55, hobby: ["football", "ski", "hiking"], type: "Admin"}, 
     {name: "Yulian", age: 28, hobby: ["films", "games", "hiking"], type: "user"}, 
@@ -49,13 +49,11 @@ for (let i of Peoples) {
     };
     console.log(i);
 };
-*/
+
 
 // 4.	Додати на сторінку івент який буде викидати алерт як тільки ми захочемо скопіювати текст з сторінки(додайте 1 рядок будь якого тексту) і сповіщати що це інтелектуальна власність власника.
-
 function functionCopy() {
-    alert("НЕ КОПІЮЙ МЕНЕ!!!!");
+    alert("НЕ КОПІЮЙ МЕНЕ!!!! All right reserved!");
 } 
-
 
 // 5.	https://restcountries.eu/rest/v2/all – АПІ endpoint для країн. Вивести нумерований список з іменами всіх країн та її столицею на сторінку, назву країни зробити великими буквами.
